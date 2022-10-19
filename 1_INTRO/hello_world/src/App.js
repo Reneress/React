437609fddc796df28afb4a047488432f7d391c3e
Leonardo Reneres <leonardoreneres525@gmail.com>
2022-10-19 01:17:27 -0300

    1-2AUla

--- 1_INTRO/hello_world/src/App.js
+++ 1_INTRO/hello_world/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p class = "primeiro">1 exercício</p>
         <a
           className="App-link"
           href="https://reactjs.org"
